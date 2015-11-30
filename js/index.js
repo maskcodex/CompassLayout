0b8e560aefcab8ace40f108d55d1296bc9a6f516
vucodex <nkanhvu90@gmail.com>
2015-11-30 14:45:11 +0700

    update buy page

--- js/index.js
+++ js/index.js
@@ -1,20 +1,45 @@
 $(document).ready(function(){
-  $(".tabstep li").click(function(event) {
-  		var clicked = $(this);
-  		var frame = clicked.find("a").attr('class');
-  	  $(".tabstep li").each(function() {
-		  $(this).removeClass("activated");
-		});
+  console.log("av=bcd");
 
-  	  clicked.addClass("activated");
-  	  $(".step-content").each(function() {
-  	  	  console.log($(this).attr('frame'));
-  	  	  if( $(this).attr('frame') !== frame) {
-  	  	  	$(this).fadeOut();	
-  	  	  }else {
-  	  	  	$(this).fadeIn();
-  	  	  }
-		});
-	});
+  $("#bed-search-filter").click(function(){
+    $(this).find(".popup").toggle();
+  });
 
+  $("#price-search-filter").click(function(){
+    $(this).find(".popup").toggle();
+  });
+
+  $("#more-search-filter").click(function(){
+    $(this).find(".popup").toggle();
+  });
+
+
+  var curleftmap = $(".listings_map_column").css("left");
+  console.log("curleftmap " + curleftmap);
+
+  $("#map-toggle").click(function(){
+    $(".map-multi-search").css("top","0");
+    $(".map-multi-search").css("left","0");
+    $(".map-multi-search").css("z-index","300");
+    $(".listings_map_column").css("left","0");
+  });
+
+  $("#list-toggle").click(function(){
+    $(".map-multi-search").css("top","0");
+    $(".map-multi-search").css("left",curleftmap);
+    $(".listings_map_column").css("left",curleftmap);
+  });
+
+  $(window).scroll(function() {
+    if ( window.pageYOffset > 81) {
+      $(".multi-search-block").css("position","fixed");
+      $(".multi-search-block").css("top","0");
+      $(".multi-search-block").css("z-index","200");
+      $(".listings_map_column").css("top","100px");
+      
+    } else {
+      $(".multi-search-block").css("position","relative");
+      $(".listings_map_column").css("top","180px");
+    }
+  });
 });
