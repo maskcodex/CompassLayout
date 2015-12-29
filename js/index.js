0e8f0272170ecf8523b65f889b87e79bff9f280e
vucodex <nkanhvu90@gmail.com>
2015-12-29 09:54:50 +0700

    update moi

--- js/index.js
+++ js/index.js
@@ -1,82 +1,117 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  $("#bed-search-filter").click(function(){
-    if ($(this).find(".popup").css("display") === "none") {
-      hide_popup_all();
-    }
-    $(this).find(".popup").toggle();
-  });
+    $("#bed-search-filter").click(function() {
+        if ($(this).find(".popup").css("display") === "none") {
+            hide_popup_all();
+        }
+        $(this).find(".popup").toggle();
+    });
 
-  $("#price-search-filter").click(function(){
-    if ($(this).find(".popup").css("display") === "none") {
-      hide_popup_all();
-    }
-    $(this).find(".popup").toggle();
-  });
+    $("#price-search-filter").click(function() {
+        if ($(this).find(".popup").css("display") === "none") {
+            hide_popup_all();
+        }
+        $(this).find(".popup").toggle();
+    });
 
-  $("#more-search-filter").click(function() {
+    $("#more-search-filter").click(function() {
 
-    if ($(this).find(".popup").css("display") === "none") {
-      hide_popup_all();
-    }
-    $(this).find(".popup").toggle();
-    
-  });
+        if ($(this).find(".popup").css("display") === "none") {
+            hide_popup_all();
+        }
+        $(this).find(".popup").toggle();
 
-  var curleftmap = $(".listings_map_column").css("left");
-  
-  $("#map-toggle").click(function(){
-    $(".map-multi-search").css("top","0");
-    $(".map-multi-search").css("left","0");
-    $(".listings_map_column").css("left","0");
-    disable_scroll();
-  });
+    });
 
-  $("#list-toggle").click(function(){
-    $(".map-multi-search").css("top","0");
-    $(".map-multi-search").css("left",curleftmap);
-    $(".listings_map_column").css("left",curleftmap);
-    enable_scroll();
-  });
+    var curleftmap = $(".listings_map_column").css("left");
 
-  $(window).scroll(function() {
-    if ( window.pageYOffset > 81) {
-      $(".multi-search-block").css("position","fixed");
-      $(".multi-search-block").css("top","0");
-    } else {
-      $(".multi-search-block").css("position","relative");
-    }
-  });
+    $("#map-toggle").click(function() {
+        $(".map-multi-search").css("top", "0");
+        $(".map-multi-search").css("left", "0");
+        $(".listings_map_column").css("left", "0");
+        disable_scroll();
+    });
 
-  $("#mobile-header-nav-icon").click(function(){
-    console.log("test");
-    var width = $( window ).width(),
-        left = width - 280 + 20;
-    $("#mobile-header-nav").css("display","block");
+    $("#list-toggle").click(function() {
+        $(".map-multi-search").css("top", "0");
+        $(".map-multi-search").css("left", curleftmap);
+        $(".listings_map_column").css("left", curleftmap);
+        enable_scroll();
+    });
 
-    $("#mobile-header-nav > ul").animate({
-        left: left +"px", 
-      }, 100,'linear', function() {
-        // Animation complete.
-      });
-      disable_scroll();
-  });
+    $(window).scroll(function() {
+        if (window.pageYOffset > 81) {
+            $(".multi-search-block").css("position", "fixed");
+            $(".multi-search-block").css("top", "0");
+        } else {
+            $(".multi-search-block").css("position", "relative");
+        }
+    });
 
-    $("#left-bg").click(function(){
-      $("#mobile-header-nav > ul").animate({
-          left: "100%",
-        }, 300,'linear', function() {
-          $("#mobile-header-nav").css("display","none");
+    $("#mobile-header-nav-icon").click(function() {
+        var width = $(window).width(),
+            left = width - 300 + 20;
+        $("#mobile-header-nav").css("display", "block");
+
+        $("#mobile-header-nav > ul").animate({
+            left: left + "px",
+        }, 100, 'linear', function() {
+            // Animation complete.
+        });
+        disable_scroll();
+    });
+
+    $("#left-bg").click(function() {
+        $("#mobile-header-nav > ul").animate({
+            left: "100%",
+        }, 300, 'linear', function() {
+            $("#mobile-header-nav").css("display", "none");
         });
         enable_scroll();
+    });
+
+    $(".cancel-mobile-menu").click(function() {
+        console.log("test");
+        $("#mobile-header-nav > ul").animate({
+            left: "100%",
+        }, 300, 'linear', function() {
+            $("#mobile-header-nav").css("display", "none");
+        });
+        enable_scroll();
+    });
+
+    $(".group-button li").click(function(event) {
+        var clicked = $(this);
+        var frame = clicked.attr('id');
+        $(".group-button li").each(function() {
+            $(this).removeClass("activated");
+        });
+
+        clicked.addClass("activated");
+
+        $(".home-info-frame").each(function() {
+            if ($(this).attr('for') !== frame) {
+                $(this).fadeOut("fast");
+            } else {
+                $(this).fadeIn("fast");
+            }
+        });
+    });
+
+    $(".mobile-search-toggle").click(function(){
+        var text = $(".mobile-search-toggle label").text() === "+" ? "x" : "+";
+        $(".price-search").toggle();
+        $(".more-search").toggle();
+        $(".bed-search").toggle();
+        $(".mobile-search-toggle label").text(text);
     });
 });
 
 
-function show_full () {
-    $(".slider-full").css("left","0");
-    $(".cancel-full-slider").click(function(){
-      $(".slider-full").css("left","999999px");
+function show_full() {
+    $(".slider-full").css("left", "0");
+    $(".cancel-full-slider").click(function() {
+        $(".slider-full").css("left", "999999px");
     });
 }
 
@@ -87,15 +122,15 @@ function disable_scroll() {
     });
 }
 
-function enable_scroll () {
-  $('html, body').css({
-      'overflow': 'auto',
-      'height': 'auto'
-  });
+function enable_scroll() {
+    $('html, body').css({
+        'overflow': 'auto',
+        'height': 'auto'
+    });
 }
 
-function hide_popup_all () {
-  $(".popup").each(function() {
+function hide_popup_all() {
+    $(".popup").each(function() {
         $(this).hide();
-  });
+    });
 }
