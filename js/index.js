1a324edc0f1431d696d760034dff9acdc3c640b7
phandung122 <phandung122@gmail.com>
2015-12-30 12:41:59 +0700

    update some js
    update some js

--- js/index.js
+++ js/index.js
@@ -169,33 +169,54 @@ function property_query (number_post) {
 }
 
 function property_query_data (number_post) {
+	var paged = $('.listing-results').data('paged');
+	var orderString = $('.search_results_sort_by option:selected').val();
 
+	var bedList = $('.bed-list');
+	var bedArray = [];
+	for( var i = 0; i<bedList.length; i++) {
+		if (bedList[i].checked == true) {
+			bedArray.push(bedList[i].value);
+		}
+	}
+	var bedArrayString = bedArray.join(",");
+	
+	
+	var priceMin = $('.priceMin').val();
+	var priceMax = $('.priceMax').val();
+	
+	console.log(bedArrayString);
 	$.ajax({
 		url : ajaxurl,
 		type : 'post',
 		data : {
 			'action' : 'ajax_property_query_data',
 			'number_post' : number_post,
-			
+			'paged'	: paged,
+			'orderby' : orderString,		
+			'bedArray' : bedArrayString,
+			'priceMin' : priceMin,
+			'priceMax' : priceMax,
 		},
 		success : function( data ) {
 			data = JSON.parse(data);
 			console.log(data);
 			
 			$('.listing-results').html(data[0]);
-			
+			$('.search_results_stats').html(data[1]);
+			$('.buy-navigation').html(data[2]);
 			var mapProp = {
 				center:new google.maps.LatLng(40.7748,-73.9617),
 				zoom:10,
 				mapTypeId:google.maps.MapTypeId.ROADMAP
 			};
 			var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-			for (var x = 1; x<data.length; x++) {
+			for (var x = 3; x<data.length; x++) {
 					
 				var myCenter= new google.maps.LatLng(data[x][7],data[x][8]);
 				var marker=new google.maps.Marker({
 					position:myCenter,
-					icon: templateDir+'/images/marker1.png'
+					icon: templateDir+'/images/pin67 (2).png'
 				});		
 				marker.setMap(map);
 			}
@@ -210,4 +231,35 @@ function property_query_data (number_post) {
 function sell_contact () {
     $('.first-home-info').hide();
     $('.sell-home-contact').show();
+}
+
+function searchOrderChanged() {
+	var orderString = $('.search_results_sort_by option:selected').val();
+	//console.log(orderString);
+	//console.log('selected changed' +  orderString);	
+	property_query_data();
+}
+
+function bedFilterChanged() {
+	
+	var bedList = $('.bed-list');
+	var bedArray = [];
+	for( var i = 0; i<bedList.length; i++) {
+		if (bedList[i].checked == true) {
+			//console.log(bedList[i].value);
+			bedArray.push(bedList[i].value);
+			console.log(bedArray);
+		}
+	}
+	
+	property_query_data();
+}
+
+function priceFilterChanged() {
+	var priceMin = $('.priceMin').val();
+	console.log(priceMin);
+	var priceMax = $('.priceMax').val();
+	console.log(priceMax);
+	
+	property_query_data();
 }
